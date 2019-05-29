fa454f9561b2121bd295c1e2abea2b1377e7b8b2
Jimmy Li <lij5@mskcc.org>
2019-05-28 22:36:36 -0400

    Initial project commit

---
+++ WhoRules/WhoRules/clientapp/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import styled from 'tachyons-components'
+const Button = styled('button')`
+  f6 f5-ns fw6 dib ba
+  b--black-20 bg-blue white
+  ph3 ph4-ns pv2 pv3-ns br2
+  grow no-underline
+`;
+function App() {
+  return (
+      <div className="App">
+          <Button mr2>Hello</Button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
