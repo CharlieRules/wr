eb4d2b9a8f5c98bc752587e79f6aacc1befe409d
Jimmy Li <lij5@mskcc.org>
2019-06-03 15:07:31 -0400

    Add logo to appbar

---
+++ WhoRules/WhoRules/clientapp/src/components/assets/index.js
@@ -0,0 +1,1 @@
+﻿export * from  "../img/WhoRulesLogo.png";
--- WhoRules/WhoRules/clientapp/src/components/header/header.jsx
+++ WhoRules/WhoRules/clientapp/src/components/header/header.jsx
@@ -1,10 +1,24 @@
 ﻿import React from 'react';
-
- const Header = () => {
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Logo from "../assets/img/WhoRulesLogo.png";
+const Header = () => {
+    const logo = () =>
+    (
+            <img src="https://unsplash.it/40/40" alt="WhoRules?!" />
+    );
     return (
-        <header>
-            Test Header
-        </header>
+        <AppBar
+           
+        >
+            <Toolbar>
+                <img src={Logo} alt="WhoRules?!" width="120px"/>
+                <Typography>
+                   WhoRules?!
+                </Typography>
+            </Toolbar>   
+        </AppBar>
     );
 }
 
