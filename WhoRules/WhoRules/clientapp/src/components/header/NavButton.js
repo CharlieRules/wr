5ce75c97901a434a37df971fe54dbab719534bdb
Jimmy Li <lij5@mskcc.org>
2019-06-03 22:43:27 -0400

    move logo to inside toolbar

---
+++ WhoRules/WhoRules/clientapp/src/components/header/NavButton.js
@@ -0,0 +1,25 @@
+﻿import React from 'react';
+
+import styled from 'tachyons-components';
+import Button from '@material-ui/core/Button';
+import Tab from '@material-ui/core/Tab';
+
+//const StyledButton = styled('Button')`
+//  f6 f5-ns fw6 dib ba
+//  b--black-20 bg-blue white
+//  ph3 ph4-ns pv2 pv3-ns br2
+//  grow no-underline
+//`;
+const StyledButton = styled('Button')`
+  white;
+	background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+`;
+
+const StyledTab = styled('Tab')`white background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);`
+const NavButton = ({ text }) => (
+    <Tab style={{color:"white", "background": "linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%)"}} label={text} >
+        
+	</Tab>
+    );
+
+ export default NavButton;
--- WhoRules/WhoRules/clientapp/src/components/header/header.jsx
+++ WhoRules/WhoRules/clientapp/src/components/header/header.jsx
@@ -2,22 +2,25 @@
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import NavButton from './NavButton';
 import Logo from "../assets/img/WhoRulesLogo.png";
+import styled from 'tachyons-components';
+const StyledToolBar = styled('Toolbar')``;
 const Header = () => {
-    const logo = () =>
-    (
-            <img src="https://unsplash.it/40/40" alt="WhoRules?!" />
-    );
+  
     return (
         <AppBar
            
         >
-            <Toolbar>
-                <img src={Logo} alt="WhoRules?!" width="120px"/>
-                <Typography>
-                   WhoRules?!
-                </Typography>
-            </Toolbar>   
+            
+
+            <StyledToolBar className="">
+                <img src={Logo} alt="WhoRules?!" width="120px" />
+                <NavButton text="About"/>
+                <NavButton text="Brands" />      
+                <NavButton text="Contact" />
+            </StyledToolBar>   
         </AppBar>
     );
 }
